86f02bf63e405b5146ca94f2224618c6bf05d2ec
Ostap Kutyanskyy <ostapkutya@gmail.com>
2018-09-17 18:11:28 +0300

    fix allocate

--- main.py
+++ main.py
@@ -15,20 +15,104 @@ def showHelpMessage():
     free <num> - free block with first cell number <num>""")
 
 
-def printFunc():
-    print("Print function")
+buff = []
 
+
+def printFunc():
+    # print("Print function")
+    vertBar = "|"
+    filler = "x"
+
+    # if not memory:
+    #     if memorySize % width == 0:
+    #         for i in range(int(memorySize / width)):
+    #             print(vertBar + "x" * (width - 1) * 2 + vertBar, end="\n")
+    #     else:
+    #         for j in range(int(memorySize // width)):
+    #             print(vertBar + " " * (width - 1) * 2 + vertBar, end="\n")
+    #         print(vertBar + "x" * ((int(memorySize % width)) * 2) + vertBar, end="\n")
+    # else:
+    #     for i in memory:
+    #         if i[0] == 0:
+    #             print(vertBar + ((i[1] * 2) - 1)  * "x" + vertBar,end="")
+    #             if()
+
+    # for i in range(len(memory)):
+    #     temp = 0
+    #     temp += memory[i][1]
+    #     if temp >= 0 and temp != :
+    #         str = vertBar + str(memory[i][0]) + filler * (temp * 2 - 1)
+    #         buff[0] += str + vertBar
+    #         # buff[0] = vertBar + filler * (temp * 2 -1 ) + vertBar + (memorySize * 2 - temp * 2) * " " + vertBar
+    #     else:
+    #         continue
+    #     if (temp > (width + 2)):
+    #         print(temp / (width + 2))
+    # print(buff[0])
+    if not buff:
+        if memorySize % width == 0:
+            segment = memorySize / width
+            for i in range(int(segment)):
+                buff.append([vertBar])
+                for j in range(width * 2):
+                    # if buff[i][0] != vertBar:
+                    #     buff[i].append(vertBar)
+
+                    buff[i].append(" ")
+        # else:
+        #     segment = memorySize // width
+        #     remainder = memorySize % width
+        #     for i in range(int(segment)):
+        #         buff.append(vertBar + " " * width * 2 + vertBar)
+        #     buff.append(vertBar + " " * int(remainder) * 2 + vertBar)
+    # print(buff[0].split(" "))
+    # buff[0] = buff[0].split()
+    # for i in range(len(buff[0])):
+    #     print(buff[0][i],end="")
+    # for i in memory:
+    #     for j in
+    # print(len(buff[0]))
+    # for i in memory:
+    #     temp = i[1] * 2
+    #     index = 0
+    #     for j in range(len(buff)):
+    #         for k in range(len(buff[j])):
+    #             if buff[j][k] == "x":
+    #                 continue
+    #             else:
+    #                 index = k
+    #                 break
+    #         # buff[j] = buff[j].replace(buff[j][1:temp], filler * temp + vertBar, 1)
+    #         # break
+    #
+    #     # temp = (
+    #     # i[1] * 2)
+    #     # old = buff[0][1:temp]
+    #     # new = filler * temp
+    #     # buff[0] = buff[0].replace(old,new,1) #vertBar + filler * temp  + vertBar + buff[0][temp+1:]
+    #
+    # print(buff[0])
+    # print(buff)
+    for i in buff:
+        for j in range(len(i)):
+            print(i[j],end="")
+        print(vertBar,end="\n")
+    # print(buff)
 
 def allocate(num):
     # print(num)
     global allocIndex
     if num > memorySize:
         return print("OUT_OF_RANGE")
-    elif num <=0:
-        return print("")
+    elif num <= 0:
+        print("""
+Cant allocate memory that have size 0 or negativ.
+Please enter yout number again.
+            """)
     elif not memory:
         allocIndex = 0
         memory.append([allocIndex, num])
+        print(allocIndex)
         allocIndex += num
     elif len(memory) >= 1:
 
@@ -36,17 +120,16 @@ def allocate(num):
             if memory[i + 1][0] - memory[i][1] != memory[i][0] and num <= memory[i + 1][0] - (
                     memory[i][1] + memory[i][0]):
                 memory.insert(i + 1, [memory[i][1] + memory[i][0], num])
-                return print(memory)
+                return print(memory[i][1] + memory[i][0])
 
         else:
-            print("sum", (memory[-1][0] + memory[-1][1]) + num)
             if (memory[-1][0] + memory[-1][1]) + num > memorySize:
-                return
+                return print("OUT_OF_RANGE")
             memory.append([allocIndex, num])
+            print(allocIndex)
             allocIndex = allocIndex + num
 
-        print(memory)
-
+        # print(memory)
 
 
 def free(num):
